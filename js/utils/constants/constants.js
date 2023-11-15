62be8daabf6612e7238b20ad05ea52eea1e19ec5
H4ckW1s3r <clementix01@gmail.com>
2023-11-16 00:37:59 +0100

    Moved addConfetti to utils, and moved constants

--- js/utils/constants/constants.js
+++ js/utils/constants/constants.js
@@ -4,10 +4,33 @@ const SPEED_RAIN = 200;
 const SPEED_USER = 400;
 const MARGINS = 50;
 
+
+const DEF_COUNT = 80;
+const DEF_GRAVITY = 800;
+const DEF_AIR_DRAG = 0.9;
+const DEF_VELOCITY = [1000, 4000];
+const DEF_ANGULAR_VELOCITY = [-200, 200];
+const DEF_FADE = 0.3;
+const DEF_SPREAD = 60;
+const DEF_SPIN = [2, 8];
+const DEF_SATURATION = 0.7;
+const DEF_LIGHTNESS = 0.6;
+
 export {
     JUMP_FORCE,
     GRAVITY,
     SPEED_RAIN,
     SPEED_USER,
     MARGINS,
+
+    DEF_AIR_DRAG, 
+    DEF_ANGULAR_VELOCITY,
+    DEF_COUNT, 
+    DEF_FADE,
+    DEF_GRAVITY,
+    DEF_LIGHTNESS, 
+    DEF_SATURATION, 
+    DEF_SPIN, 
+    DEF_SPREAD, 
+    DEF_VELOCITY,
 };
