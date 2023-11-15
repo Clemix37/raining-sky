66fb70ee8034eb32be55fe7bc74d939019462d77
H4ckW1s3r <37782019+Clemix37@users.noreply.github.com>
2023-11-15 13:34:48 +0100

    Added spawnPowerUp method

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -18,4 +18,22 @@ function spawnStar() {
     });
 }
 
-export {spawnStar};
+function spawnPowerUp(){
+    // add star
+    add([
+        rect(20, 20, {radius:50}),
+        area(),
+        outline(2, Color.fromArray([255,255,255])),
+        pos(rand(0, width() - MARGINS), 5),
+        anchor("topleft"),
+        color(0, 0, 255),
+        move(DOWN, SPEED_RAIN),
+        offscreen({ destroy: true }),
+        "POWER", // add a tag here
+    ]);
+    wait(rand(2, 5), () => {
+        spawnPowerUp();
+    });
+}
+
+export { spawnStar? spawnPowerUp };
